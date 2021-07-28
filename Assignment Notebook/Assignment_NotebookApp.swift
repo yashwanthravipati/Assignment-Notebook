a4a563fe9c3c946b7ce5a12e2cc3e8f076e5abd6
Yashwanth Ravipati <yashwanth.ravipati@icloud.com>
2021-07-28 12:00:41 -0500

    Initial Commit

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by Yashwanth Ravipati on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
